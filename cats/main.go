a5d188e9b71e65074e659d26da8f34dbca42a835
sa1kri <92201506+sa1kri@users.noreply.github.com>
2022-12-17 14:26:53 +0500

    Add files via upload

---
+++ cats/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type Cat struct {
+	ID string `json:"id"`
+	Name string `json:"name"`
+	InStrip bool `json:"is_strip"`
+	Color string `json:"color"`
+}
+
+func main() {
+	var cats []Cat
+
+	r := gin.Default()
+
+	r.POST("/api/cat/add", func(c *gin.Context) {
+
+		var cat Cat
+
+		if err := c.BindJSON(&cat); err != nil {
+			return
+		}
+		cat.ID = uuid.NewString()
+		cats = append(cats, cat)
+
+		c.JSON(200, cat)
+	})
+
+	r.GET("/api/cat/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var cat Cat
+		
+		for _,ct := range cats {
+			if ct.ID == id {
+				cat = ct
+			}
+		}
+
+		c.JSON(200, cat)
+	})
+
+	r.DELETE("/api/cat/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var index int
+		var cat Cat
+		
+		for i,ct := range cats {
+			if ct.ID == id {
+				index = i
+				cat = ct
+			}
+		}
+
+		cats = append(cats[:index], cats[index+1:]...)
+
+		c.JSON(200, cat)
+	})
+	
+
+	r.Run("0.0.0.0:8888")
+}
